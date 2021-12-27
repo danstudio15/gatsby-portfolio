88c9df5ae9cf52b9db7cdd04945f3e90edcd2cf9
Danny Cooling <daniel.alexander.cooling@gmail.com>
2021-12-27 17:53:09 +0000

    Up to lesson 11

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Link } from 'gatsby'
 import React from 'react'
 
@@ -12,4 +13,31 @@ export default function Navbar() {
             </div>
         </nav>
     )
+=======
+import React from 'react'
+import { graphql, Link, useStaticQuery } from 'gatsby'
+
+export default function Navbar() {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  const { title } = data.site.siteMetadata
+
+  return (
+    <nav>
+      <h1>{ title }</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Portfolio Projects</Link>
+      </div>
+    </nav>
+  )
+>>>>>>> Stashed changes
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+<<<<<<< Updated upstream
 
 export default function NotFound() {
     return (
@@ -8,3 +9,19 @@ export default function NotFound() {
         </div>
     )
 }
+=======
+import Layout from '../components/Layout'
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <div>
+        <h2>404</h2>
+        <p>Sorry, that page doesn't exist (yet)!</p>
+      </div>
+    </Layout>
+  );
+}
+ 
+export default NotFound
+>>>>>>> Stashed changes
