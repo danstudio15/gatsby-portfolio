5162f3b524911f7b7f7917f183d958e5515ae42e
Danny Cooling <daniel.alexander.cooling@gmail.com>
2022-02-15 22:42:13 +0000

    test3

--- src/pages/projects/my-life-in-weeks.js
+++ src/pages/projects/my-life-in-weeks.js
@@ -8,10 +8,10 @@ const TimestampToDays = (time) => {
 
 const MyLifeInWeeks = ({location}) => {
 
-    let inputValue = new Date().toString();
-    if (location.state.data) {
-        inputValue = location.state.data[0];
-    }
+    let inputValue = new Date();
+    // if (location.state.data) {
+    //     inputValue = location.state.data[0];
+    // }
     // let inputValue = (location.state.data) ? location.state.data[0] : new Date();
     const dob = Date.parse(inputValue);
     const date = Date.parse(new Date());
